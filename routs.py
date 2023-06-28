75ecc87e0d04e713e91ef344b1f08838deea6144
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2023-06-29 09:16:45 +1200

    Updated database. added form system to courses. re did the routes to link better with the couses

--- routs.py
+++ routs.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sqlite3
 
 app = Flask (__name__)
@@ -7,11 +7,57 @@ app = Flask (__name__)
 def home():
     return render_template("home.html", title = "Home Page")
 
+@app.route('/courses/add', methods=['POST'])
+def add_course():
+    name = request.form['name']
+    location = request.form['location']
+    description = request.form['description']
+    par = int(request.form['par'])
+    yardage = int(request.form['yardage'])
+    rating = float(request.form['rating'])
+    slope = int(request.form['slope'])
+
+
+    conn = sqlite3.connect('golfweb.db')
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO Courses (name, location, description, par, yardage, rating, slope) VALUES (?, ?, ?, ?, ?, ?, ?)',
+                   (name, location, description, par, yardage, rating, slope))
+    conn.commit
+
+
+    return 'Course added successfully!'
+
+@app.route('/courses')
+def courses():
+    # Assuming you have a list of courses to display
+    courses = [...]
+    
+    # Assuming you have a route for adding courses
+    add_course_route = '/courses/add'
+
+    return render_template('courses.html', courses=courses, add_course_route=add_course_route)
+
+
+# @app.route('/courses')
+# def courses():
+  #  conn = sqlite3.connect('golfweb.db')
+   # cursor = conn.cursor()
+    #cursor.execute('SELECT * FROM Courses')
+    #courses = cursor.fetchall()
+
+    # Process the courses data and display it on your website
+    # For example, you can pass the data to a template for rendering
+
+    return render_template('courses.html', courses=courses)
+
+
+
+
 @app.route("/all_courses")
 def all_courses():
     conn= sqlite3.connect("golfweb.db")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM Course")
+    cur.execute("SELECT * FROM Courses")
     results = cur.fetchall()
     return render_template("all_courses.html", results = results , title = "All Courses")
 
@@ -24,10 +70,10 @@ def contact():
 def golf (id):
     conn = sqlite3.connect('golfweb.db')
     cur = conn.cursor()
-    cur.execute ('SELECT * FROM Course WHERE id=?', (id,))
+    cur.execute ('SELECT * FROM Courses WHERE id=?', (id,))
     golf = cur.fetchone()
     print(golf)
-    cur.execute('SELECT Name FROM Course WHERE id=?', (golf [2],))
+    cur.execute('SELECT Name FROM Courses WHERE id=?', (golf [2],))
     review = cur.fetchone()
     return render_template( 'golf.html', golf=golf , review=review)
 
